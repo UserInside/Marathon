51721a1f4213c77da3ae5f1e20a73cc738ff7cee
Voronov Igor <voronov.i.v@mail.ru>
2022-08-07 16:26:47 +0300

    complete day 19 tasks

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,54 @@
 package day19;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.*;
+
 /**
  * @author Neil Alishev
  */
 public class Task1 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/main/resources/dushi.txt");
+        Scanner scanner = new Scanner(file);
+        scanner.useDelimiter("[.,:;()?!\"\\s–]+");
 
+        Map<String, Integer> words = new HashMap<>();
+
+        while (scanner.hasNext()) {
+
+            String s = scanner.next();
+
+            if (words.containsKey(s)) {
+                words.put(s, words.get(s) + 1);
+            } else {
+                words.put(s, 1);
+            }
+        }
+        scanner.close();
+
+        // TODO отсортировать карту по значениям
+
+        int counter = 0;
+        while (counter != 100) {
+            int maxWordValue = 0;
+            for (Map.Entry<String, Integer> word : words.entrySet()) {
+                if (word.getValue() > maxWordValue) {
+                    maxWordValue = word.getValue();
+                }
+            }
+
+            for (Map.Entry<String, Integer> word : words.entrySet()) {
+                if (word.getValue() == maxWordValue) {
+                    System.out.println(word);
+                    word.setValue(0);
+                }
+            }
+
+
+            counter++;
+        }
+
+        //Чичиков - 601
     }
 }
--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -1,10 +1,48 @@
 package day19.task2;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 /**
  * @author Neil Alishev
  */
 public class Task2 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws FileNotFoundException {
+        File file = new File("src/main/resources/taxi_cars.txt");
+        Scanner scanner = new Scanner(file);
+
+        Map<Integer, Point> cars = new HashMap<>();
+
+        while (scanner.hasNextLine()) {
+            String[] s = scanner.nextLine().split(" ");
+
+            Point point = new Point(Integer.parseInt(s[1]), Integer.parseInt(s[2]));
+
+            cars.put(Integer.parseInt(s[0]), point);
+        }
+
+        Scanner input = new Scanner(System.in);
+        System.out.println("Введите четыре числа");
+
+        int x1 = input.nextInt();
+        int y1 = input.nextInt();
+        int x2 = input.nextInt();
+        int y2 = input.nextInt();
+
+        int counter = 0;
+
+        for (Map.Entry<Integer, Point> car : cars.entrySet()) {
+            if (car.getValue().getX() > x1 && car.getValue().getY() > y1
+                    && car.getValue().getX() < x2 && car.getValue().getY() < y2) {
+                System.out.println("Машина номер " + car.getKey() + " внутри квадрата");
+                counter += 1;
+            }
+        }
+        System.out.println();
+        System.out.println("Всего внутри квадрата " + counter + " машин");
 
     }
 }
